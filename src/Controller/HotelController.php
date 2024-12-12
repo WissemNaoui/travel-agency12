185e0c87028a241e0a158ec9cbc059b46aa7317c
WissemNaoui <wissem.naoui@esen.tn>
2024-12-12 10:00:51 +0100

    now we can see hotels

--- src/Controller/HotelController.php
+++ src/Controller/HotelController.php
@@ -2,27 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Hotel;
+use App\Repository\HotelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 #[Route('/hotels')]
 class HotelController extends AbstractController
 {
+    private $hotelRepository;
+
+    public function __construct(HotelRepository $hotelRepository)
+    {
+        $this->hotelRepository = $hotelRepository;
+    }
+
     #[Route('', name: 'app_hotels')]
     public function index(): Response
     {
+        $hotels = $this->hotelRepository->findAll();
+        
         return $this->render('hotel/index.html.twig', [
-            'hotels' => [], // TODO: Add hotel repository and fetch hotels
+            'hotels' => $hotels,
         ]);
     }
 
     #[Route('/{id}', name: 'app_hotel_show', requirements: ['id' => '\d+'])]
     public function show(int $id): Response
     {
-        // TODO: Add hotel repository and fetch hotel by id
+        $hotel = $this->hotelRepository->find($id);
+        
+        if (!$hotel) {
+            throw $this->createNotFoundException('Hotel not found');
+        }
+
         return $this->render('hotel/show.html.twig', [
-            'hotel' => null,
+            'hotel' => $hotel,
         ]);
     }
 }
